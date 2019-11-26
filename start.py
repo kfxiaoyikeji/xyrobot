ad7f1e785147c1f3b8db0f75f49893ef532e6677
yi <findlymw@gmail.com>
2019-11-26 19:45:03 +0800

    增加小姐姐的唤醒pmdl,增加训练唤醒词方法,修改start.py KWS的参数为自定义的pmdl,这里需要绝对路径

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import time
+import os
 from common import config, constants
 from voice_engine.source import Source
 from voice_engine.channel_picker import ChannelPicker
@@ -7,10 +8,15 @@ from voice_engine.kws import KWS
 from voice_engine.doa_respeaker_4mic_array import DOA
 from commponents.kws.pixels import pixels
 
+# Base64编码是一种“防君子不防小人”的编码方式 生成的编码可逆，后一两位可能有“=”，生成的编码都是ascii字符
+import base64
+# requests库是一个常用的用于http请求的模块，它使用python语言编写，可以方便的对网页进行爬取，是学习python爬虫的较好的http请求模块。
+import requests
+
 class XYRobot(object):
     src = Source(rate=16000, channels=4, frames_size=320)
     ch1 = ChannelPicker(channels=4, pick=1)
-    kws = KWS()
+    kws = KWS(os.path.join(constants.DATA_PATH ,config.get('kws_file_name','')))
     doa = DOA(rate=16000)
     
     def __init__(self):
@@ -25,17 +31,45 @@ class XYRobot(object):
             
             print('detected {} at direction {} is {}'.format(keyword, position,direction))
         self.kws.set_callback(on_detected)
+        config.init()       
+        
+    def run(self):
         self.src.recursive_start()
-    
         while True:
             try:
                 time.sleep(1)
             except KeyboardInterrupt:
                 break
-    
         self.src.recursive_stop()
         
-        config.init()       
+    def train(self, w1, w2, w3, m):
+            '''
+            传入三个wav文件，生成snowboy的.pmdl模型
+            '''
+            def get_wave(fname):
+                with open(fname, 'rb') as infile:
+                    return base64.b64encode(infile.read()).decode('utf-8')
+            url = 'https://snowboy.kitt.ai/api/v1/train/'
+            data = {
+                "name": "wukong-robot",
+                "language": "zh",
+                "token": config.get('snowboy_token', ''),
+                "voice_samples": [
+                    {"wave": get_wave(w1)},
+                    {"wave": get_wave(w2)},
+                    {"wave": get_wave(w3)}
+                ]
+            }
+            
+            response = requests.post(url, json=data)
+            print(response.ok)
+            if response.ok:
+                with open(m, "wb") as outfile:
+                    outfile.write(response.content)
+                return 'Snowboy模型已保存至{}'.format(m)
+            else:
+                return "Snowboy模型生成失败，原因:{}".format(response.text)
+        
 if __name__ == '__main__':
     print('''
 ********************************************************
@@ -46,3 +80,8 @@ if __name__ == '__main__':
               ''')
     print(config.get('xyrobot_name'))
     xyRobot = XYRobot()
+    '''
+    xyRobot.train('/home/pi/Documents/workspace/xyrobot/staticData/xiaojiejie.wav','/home/pi/Documents/workspace/xyrobot/staticData/xiaojiejie1.wav','/home/pi/Documents/workspace/xyrobot/staticData/xiaojiejie2.wav','/home/pi/Documents/workspace/xyrobot/staticData/xiaojiejie.pmdl')
+    '''
+    xyRobot.run()
+    
